d430d4dc5b7be51e4804c5cf30c675db1c681314
ArthurHallack <ArthurHallack2@gmail.com>
2024-08-26 23:10:06 -0300

    Yoshaa

--- resources/views/sorteio.blade.php
+++ resources/views/sorteio.blade.php
@@ -4,31 +4,33 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Sorteio</title>
+    <link rel="stylesheet" href="/css/sorteio.css">
 </head>
 <body>
     <header>
-        <nav>
-            <h1>Amigo Secreto</h1>
-        </nav>
+            <nav>
+                <h1>Amigo Secreto</h1>
+                <a id="BtnSorteio" href="{{ route('home') }}">Home</a>
+            </nav>
     </header>
     <main>
-    <h1>Resultado do Sorteio de Amigo Secreto</h1>
-    
-    @if(!empty($pares))
-        <ul>
-            @foreach($pares as $par)
-                <li>
-                    @if($par[1])
-                        {{ $par[0]->nome }} está sorteado para {{ $par[1]->nome }}
-                    @else
-                        {{ $par[0]->nome }} não tem par
-                    @endif
-                </li>
-            @endforeach
-        </ul>
-    @else
-        <p>Não há participantes para o sorteio.</p>
-    @endif
+        <h1>Resultado do Sorteio de Amigo Secreto</h1>
+        
+        @if(!empty($pares))
+            <ul>
+                @foreach($pares as $par)
+                    <li>
+                        @if($par[1])
+                            {{ $par[0]->nome }} está sorteado para {{ $par[1]->nome }}
+                        @else
+                            {{ $par[0]->nome }} não tem par
+                        @endif
+                    </li>
+                @endforeach
+            </ul>
+        @else
+            <p>Não há participantes para o sorteio.</p>
+        @endif
     </main>
 </body>
 </html>
