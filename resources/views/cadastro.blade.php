28d4575c8f48e13415d25bf68a229c26b4949746
ArthurHallack <ArthurHallack2@gmail.com>
2024-08-27 01:12:23 -0300

    Yoshaa

--- resources/views/cadastro.blade.php
+++ resources/views/cadastro.blade.php
@@ -6,7 +6,7 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>Cadastro</title>
     <link rel="stylesheet" href="/css/cadastro.css">
-    <script src="/js/cadastro.js"></script>
+    <script src="/js/cadastro.js" defer></script>
 </head>
 <body>
     <header>
@@ -20,21 +20,29 @@
             <h1>Cadastro</h1>
         </div>
         <form method="POST" action="{{ route('participantes.store') }}" id="formAdd">
-            @csrf
-            <div id="inputsArea">
-                <fieldset>
-                    <label for="inputNome">Nome</label>
-                    <input type="text" id="inputNome" name="nome" required>
-                </fieldset>
-                <fieldset>
-                    <label for="inputEmail">Email</label>
-                    <input type="text" id="inputEmail" name="email" required>
-                </fieldset>
-            </div>
-            <div>
-                <button type="submit" id="BtnSave">Salvar</button>
-            </div>
-        </form>
+    @csrf
+    <div id="inputsArea">
+        <fieldset>
+            <label for="inputNome">Nome</label>
+            <input type="text" id="inputNome" name="nome" minlength="3" maxlength="15" required>
+        </fieldset>
+        <fieldset>
+            <label for="inputEmail">Email</label>
+            <input type="text" id="inputEmail" name="email" minlength="12" maxlength="60" required>
+        </fieldset>
+    </div>
+    <div>
+        <button type="submit" id="BtnSave">Salvar</button>
+        <div id="error-message" style="color: red; display: none;">
+            <p></p>
+        </div>
+        <div id="success-message" style="color: green; display: none;">
+            <p></p>
+        </div>
+    </div>
+</form>
+
+
     </main>
 
 </body>
